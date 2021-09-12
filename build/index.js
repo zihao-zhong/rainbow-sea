b5c82769401da314cad4a587ce15eadeba376a7d
v_zihzhong <v_zihzhong@tencent.com>
2021-09-12 16:07:00 +0800

    feat: 修改构建脚本2

--- build/index.js
+++ build/index.js
@@ -12,9 +12,10 @@ function handleBuild() {
   shell.exec('npm run build');
 
   shell.echo(chalk.green('项目打包完成, 配置 Nginx'));
-  // shell.exec('rm -rf /usr/local/nginx/html/*');
-  // shell.exec('mv ./dist/* /usr/local/nginx/html');
-  // shell.exec('nginx -s reload');
+  shell.exec('rm -rf /usr/local/nginx/html/*');
+  shell.exec('mv ./dist/* /usr/local/nginx/html');
+  shell.exec('nginx -s reload');
   shell.echo(chalk.green('项目构建完成'));
+  shell.echo(chalk.green('http://www.zihao.work'));
   shell.exit();
 }
