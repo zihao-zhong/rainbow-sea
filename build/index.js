bfaece4b58311f43897257aaa4cabc35c58feee6
zihao <1095996920@qq.com>
2021-09-25 21:52:32 +0800

    feat: 构建脚本增加错误兜底

--- build/index.js
+++ build/index.js
@@ -6,16 +6,23 @@ handleBuild();
 
 
 function handleBuild() {
-  shell.echo(chalk.green('开始打包...'));
-  shell.exec('git pull');
-  shell.exec('npm install');
-  shell.exec('npm run build');
+  try {
+    shell.echo(chalk.green('拉取远程代码'));
+    shell.exec('git pull');
+    shell.echo(chalk.green('下载依赖包'));
+    shell.exec('npm install');
+    shell.echo(chalk.green('开始打包'));
+    shell.exec('npm run build');
 
-  shell.echo(chalk.green('项目打包完成, 配置 Nginx'));
-  shell.exec('rm -rf /usr/local/nginx/html/*');
-  shell.exec('mv ./dist/* /usr/local/nginx/html');
-  shell.exec('nginx -s reload');
-  shell.echo(chalk.green('项目构建完成'));
-  shell.echo(chalk.green('https://www.zihao.work'));
-  shell.exit();
+    shell.echo(chalk.green('项目打包完成, 配置 Nginx'));
+    shell.exec('rm -rf /usr/local/nginx/html/*');
+    shell.exec('mv ./dist/* /usr/local/nginx/html');
+    shell.exec('nginx -s reload');
+    shell.echo(chalk.green('项目构建完成'));
+    shell.echo(chalk.green('https://www.zihao.work'));
+    shell.exit();
+  } catch (error) {
+    console.error(error);
+    shell.exit(1);
+  }
 }
